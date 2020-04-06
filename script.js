302f6c4453271812851f5828c9df1da7266177a4
Sielik <kr.sielicki@gmail.com>
2020-04-06 09:06:47 +0200

    commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let container = document.querySelector('.images');
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
+
+let counter = 0;
+
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+
+function prevSlide() {
+
+    if (counter === 4) {
+        counter = 0;
+    }
+
+    counter ++;
+    container.style.backgroundImage = `url(img/bcg-${counter}.jpg`;
+    container.animate([{opacity: '0.2'},{opacity: '1.0'}], {duration: 1000, fill: 'forwards'});
+
+}
+function nextSlide() {    
+
+    if (counter === 0) {
+        counter = 4;
+    }
+
+    counter --;
+    container.style.backgroundImage = `url(img/bcg-${counter}.jpg`;
+    container.animate([{opacity: '0.2'},{opacity: '1.0'}], {duration: 1000, fill: 'forwards'});
+
+}
